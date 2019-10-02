45aa3ff26fea15544a2dfd6d770a78d0f028686d
rafaewo <rafaelnogcunha@hotmail.com>
2019-10-02 13:24:10 -0300

    adicionando o inicio do backend

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+// mongoose.connect('', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// })
+
+// req.query = acessar query params (para filtros)
+// req.params = acessar route params (para edição, delete)
+// req.body = acessar o corpo da requisição (para criar e editar)
+
+app.use(express.json());
+app.use(routes);
+
+
+app.listen(3333);
